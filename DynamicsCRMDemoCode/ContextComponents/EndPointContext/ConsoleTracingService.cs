dfc8fa6853d599aa2e585ed263bcf83f8ac6a19b
arnongold <arnon.public@gmail.com>
2017-06-08 13:35:00 +0300

    added standalone context

---
+++ DynamicsCRMDemoCode/ContextComponents/EndPointContext/ConsoleTracingService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicsCRMDemoPlugin.ContextComponents.EndPointContext
+{
+    public class ConsoleTracingService : ITracingService
+    {
+        public void Trace(string format, params object[] args)
+        {
+            Console.WriteLine(string.Format(format,args));
+        }
+    }
+}
---
+++ DynamicsCRMDemoCode/ContextComponents/EndPointContext/StandAloneContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicsCRMDemoPlugin.ContextComponents.EndPointContext
+{
+    class StandAloneContext : IEndpointContext
+    {
+        public ITracingService TracingService { get; set; }
+        private IOrganizationService OrganizationService;
+        public StandAloneContext(string Url, string UserName, string Password,string autoType)
+        {
+            var connectionString = $"AuthType={autoType};Url={Url}; Username={UserName}; Password={Password}";
+            TracingService = new ConsoleTracingService();
+            CrmServiceClient crmConn = new CrmServiceClient(connectionString);
+            OrganizationService = crmConn.OrganizationServiceProxy;
+        }
+        public PassThroughContext ToPassthroughContext()
+        {
+            var passThroughContext = new PassThroughContext(OrganizationService, TracingService);
+            return passThroughContext;
+        }
+    }
+}
